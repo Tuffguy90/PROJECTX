f45b6cf1e89bf824d47a44e8e4f48c743eebd02d
Shubham Raj <rajs86599@gmail.com>
2023-03-26 21:04:01 +0530

    change.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -17,7 +17,8 @@ const { Op } = require("sequelize");
  */
 const login = async (req, res) => {
   try {
-    const email = req.body.email
+    console.log("db", db.tbl_role_masters);
+    const email = req.body.email;
     const conditions = {
       attributes: [
         "id",
@@ -37,9 +38,10 @@ const login = async (req, res) => {
       where: {
         email: email,
       },
-    }
+      logging:true
+    };
 
-    const user = await USER.findOne(conditions)
+    const user = await USER.findOne(conditions);
     if (user) {
       const isMatch = await bcrypt.compare(req.body.password, user.password);
       // if (!isMatch) {
@@ -64,7 +66,7 @@ const login = async (req, res) => {
         },
         process.env.SECRET_KEY,
         { expiresIn: "48h" }
-      )
+      );
       await USER.update(
         {
           token,
@@ -81,20 +83,20 @@ const login = async (req, res) => {
         message: "Login Success",
         user: user,
         token,
-      })
+      });
     }
     return res.status(400).send({
       status: 400,
       message: "Invalid Login Credentials",
       user: {},
-    })
+    });
   } catch (err) {
     return res.status(500).json({
       message: "Internal Server Error",
       error: err?.message,
-    })
+    });
   }
-}
+};
 
 /**
  * Change User Password
@@ -108,59 +110,59 @@ const changePassword = async (req, res) => {
       where: {
         id: req.body?.created_by,
       },
-    }
-    const user = await USER.findOne(conditions)
+    };
+    const user = await USER.findOne(conditions);
     if (user) {
       const isMatch = await bcrypt.compare(
         req.body.current_password,
         user.password
-      )
+      );
       if (!isMatch) {
         return res.status(400).send({
           status: 400,
           message: "Current Password not matched.",
-        })
+        });
       }
 
       if (req.body.new_password === req.body.current_password) {
         return res.status(400).send({
           status: 400,
           message: "Current Password and New Password cannot same.",
-        })
+        });
       }
 
       if (req.body.new_password !== req.body.confirm_password) {
         return res.status(400).send({
           status: 400,
           message: "New Password and Confirm Password must be same.",
-        })
+        });
       }
 
-      let salt = await bcrypt.genSalt(10)
-      let hashNewPassword = await bcrypt.hash(req.body.new_password, salt)
+      let salt = await bcrypt.genSalt(10);
+      let hashNewPassword = await bcrypt.hash(req.body.new_password, salt);
       await USER.update(
         { password: hashNewPassword, updatedBy: req.body.created_by },
         { where: { id: user?.id } }
-      )
+      );
       return res.send({
         status: 200,
         message: "Password Change Successfully",
         data: [],
-      })
+      });
     }
     return res.status(400).send({
       status: 400,
       message: "Something Went Wrong",
       user: {},
-    })
+    });
   } catch (err) {
     return res.status(500).json({
       message: "Internal Server Error",
       error: err?.message,
       data: [],
-    })
+    });
   }
-}
+};
 
 /**
  * To Create User (Email-Id unique)
@@ -170,9 +172,9 @@ const changePassword = async (req, res) => {
  */
 const createUser = async (req, res) => {
   try {
-    let tokenUserData = extractTokenInfo(req)
-    let body = req.body
-    let key = body.key || null
+    let tokenUserData = extractTokenInfo(req);
+    let body = req.body;
+    let key = body.key || null;
     if (key !== null) {
       body.email && delete body.email;
       body.mobile && delete body.mobile;
@@ -181,48 +183,48 @@ const createUser = async (req, res) => {
         return res.status(200).send({
           data: [],
           message: "Data Updated Successfully",
-        })
+        });
       }
     }
     /** Password will user mobile number
      */
-    body.password = req.body.mobile
-    const validate = createUserSchema.validate(body)
+    body.password = req.body.mobile;
+    const validate = createUserSchema.validate(body);
     if (validate?.error) {
       return res.status(400).send({
         message: "Validation Error",
         error: validate?.error,
-      })
+      });
     }
     const isDuplicate = await USER.findOne({
       where: {
         email: body.email,
       },
-    })
+    });
 
     if (isDuplicate) {
       return res.status(409).send({
         message: "Duplicate Data found",
         data: isDuplicate,
-      })
+      });
     }
-    body.createdBy = tokenUserData?.id || null
-    let salt = await bcrypt.genSalt(10)
-    body.password = await bcrypt.hash(body.password, salt)
-    const createdUser = await USER.create(body)
+    body.createdBy = tokenUserData?.id || null;
+    let salt = await bcrypt.genSalt(10);
+    body.password = await bcrypt.hash(body.password, salt);
+    const createdUser = await USER.create(body);
 
     return res.status(200).send({
       message: "User Created Successfully",
       data: createdUser,
-    })
+    });
   } catch (err) {
-    console.log("err", err?.message)
+    console.log("err", err?.message);
     return res.status(500).json({
       message: err?.message,
       data: [],
-    })
+    });
   }
-}
+};
 
 /**
  *
@@ -256,7 +258,7 @@ const userList = async (req, res) => {
           as: "subsidary",
         },
       ],
-      logging:true,
+      logging: true,
       where: whereCondition,
     };
     const countData = await USER.findAll(conditions);
@@ -269,17 +271,17 @@ const userList = async (req, res) => {
     return res.status(200).send({
       message: "",
       data: { data: userList, totalCount: countData?.length },
-    })
+    });
   } catch (err) {
-    return res.status(500).send({ message: err?.message, data: [] })
+    return res.status(500).send({ message: err?.message, data: [] });
   }
-}
+};
 
 const userRoutes = {
   login,
   changePassword,
   createUser,
   userList,
-}
+};
 
-module.exports = userRoutes
+module.exports = userRoutes;
